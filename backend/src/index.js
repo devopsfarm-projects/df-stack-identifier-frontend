2a8791f2c4c67d39d20688df9b9f900d2d5290b3
Rahul-Chaudhary9760 <151906773+Rahul-Chaudhary9760@users.noreply.github.com>
2024-03-01 17:36:54 +0530

    Make route for github contents

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,6 @@ const exchangeCode = async (code) => {
                 Accept: 'application/json'
             }
         })
-        console.log('response' , response.data)
         return response.data;
     } catch (error) {
         console.error("Error in Exchange code for exchange accessToken" , error);
@@ -32,18 +31,13 @@ const exchangeCode = async (code) => {
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-
-
 app.get('/api/getAccessToken' , async(req , res) => {
     try{
         const code = req.query.code;
-        console.log("Authentication code is " , code);
         if(!code){
             throw new Error("No code parameter is recieved")
         }
         const accessTokenData = await exchangeCode(code);
-        console.log("AcccesToken Data" ,  accessTokenData)
-        const accessToken =await  accessTokenData.access_token;
         res.send(accessTokenData)    
     }  catch(error){
     console.error('Error' , error.message);
@@ -87,7 +81,6 @@ app.get("/api/userInfoData" , async(req , res) => {
 app.get("/api/getRepos" , async(req,res) => {
     try {
         const authorizationHeader = req.get("Authorization");
-        console.log("Authorization Header" , authorizationHeader)
         if(!authorizationHeader){
             return res.status(401).json({ error: "Authorization header is missing" });
         }
@@ -114,23 +107,66 @@ app.get("/api/repo-contents" , async(req ,res) => {
     }
     try {
         const username = "Rahul-Chaudhary9760";
-        const repoName = "Devops_Farm";
-        // const {username , repoName} = req.query;
+        const repoName = "My-Portfolio";
         const url = `https://api.github.com/repos/${username}/${repoName}/contents`;
-        console.log("repo-content url" , url)
         const response =await axios.get(url , {
             headers : {
                 "Authorization" : `${authorizationHeader}`,
                 "Accept": "application/vnd.github+json"
             }
         })
-        const names = response.data.map(item => item.name)
-        console.log("Contents name " , names)
-        // res.json(response);
-    } catch (error) {
-        console.error("Error in getting Repo contents" , error)
+
+        const rootDirectoryContents = response.data;
+        const rootDirectoryFiles  =[];
+
+        for(const item of rootDirectoryContents){
+            if(item.type === "file" || item.type==="blob") {
+                rootDirectoryFiles.push(item.name)
+            }else if(item.type === "dir"){
+                const directoryFiles  = await collectFilesInDirectory(username , repoName , item.sha , authorizationHeader);
+                rootDirectoryFiles.push(...directoryFiles ); 
+            }
+
+        }
+        console.log("All files content" ,rootDirectoryFiles)
+    }catch(error){
+        console.error("Error in getting Repo contents", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
+    
 })
+
+async function collectFilesInDirectory(username, repoName, treeSha , authorizationHeader)  {
+    try {
+        const url = `https://api.github.com/repos/${username}/${repoName}/git/trees/${treeSha}`;
+        const response = await axios.get(url , {
+            headers : {
+                "Authorization" : `${authorizationHeader}`,
+                "Accept": "application/vnd.github+json"
+            }   
+        });
+        const subDirectoryContents  = response.data.tree;
+        const subDirectoryFiles  = []
+        for (const item of subDirectoryContents ) {
+            if (item.type === "blob"|| item.type === "file") {
+                // If it's a file, collect its name
+                subDirectoryFiles.push(item.path);
+            } else if (item.type === "tree") {
+                // If it's a directory, recursively collect files in the subdirectory
+                
+                const filesInSubDirectory  = await collectFilesInDirectory(username , repoName, item.sha);
+                subDirectoryFiles.push(...filesInSubDirectory);
+
+            }
+        }
+
+        return subDirectoryFiles;
+    } catch (error) {
+        console.error("Error collecting files in directory:", error);
+        return []; // Return an empty array if there's an error
+    }
+}
+
 
 const port = process.env.PORT || 5000 ;
 app.listen(port, () => {
